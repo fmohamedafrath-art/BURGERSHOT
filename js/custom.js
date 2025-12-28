97a8cac47ef103f3ab0893148cd88bc9ab1b9eb8
Afrath_Prince <fmohamedafrath5@gmail.com>
2025-12-28 10:16:01 +0530

    images

--- js/custom.js
+++ js/custom.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(8.493507654789322, 81.18444056719198),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
